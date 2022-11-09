2e3d3b551885547d35d22dd465b5476a95dc8f3f
Gareth Flynn <gareth.t.flynn@gmail.com>
2022-11-08 20:48:50 -0500

    added models

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose')
+const thoughtSchema = require('./Thought')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true, 
+            trimmed: true,
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            match: [
+                /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please Enter Valid Email Address."
+            ]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'  
+            }
+        ],
+        friends: [
+            {
+             type: Schema.Types.ObjectId,
+             ref: 'User'   
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema
+.virtual('friendCount')
+.get (function () {
+    return this.freinds.length
+})
+
+const User = model('User', userSchema)
+model.exports = User;
